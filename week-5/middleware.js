46136b03a9bc39da3c30099f860b53b4627afa54
Seyi0 <makanjuolaseyi560@gmail.com>
2022-09-22 22:25:49 +0100

    Main

---
+++ week-5/middleware.js
@@ -0,0 +1,16 @@
+const sayHi =(req, res, next)=>{
+    const name = `try`
+    console.log(`Hi ${name}`)
+    
+    next()
+    }
+    
+    //errror handling middleware
+    
+    const handleError =(err, req, res, next)=>{
+        console.log(err)
+    
+        next()
+    }
+    
+    module.exports = {sayHi, handleError}
