e7758bc6c35e36f47450a1889e2b94893d4297c0
Seyi0 <makanjuolaseyi560@gmail.com>
2022-10-04 11:53:34 +0100

    new commit

---
+++ week-6/index.js
@@ -0,0 +1,14 @@
+// introduction to MongoDB
+
+/*
+1. Relational database- are stored in table examples are MySql, Postgres
+2. Non-relational database- are stored in document example Mongodb
+
+a record in a mongodb database is known as documents
+Multiple documents are known as collection 
+multiple collections are known as database
+Multiple databases are known as cluster
+
+Data are stored in BSON format
+M V C 
+*/
